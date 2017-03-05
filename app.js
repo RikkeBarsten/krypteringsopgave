67e95f2e50daf6618b4aed30d586bc2a4a144ff7
Rikke Barsten <Rikke Barsten>
2017-03-05 13:34:51 +0100

    Add d3 referencetext barchart

--- app.js
+++ app.js
@@ -56,5 +56,54 @@ function frekvens(){
         }
         console.log(JSON.stringify(letterCountArray));
 
+        (function (data)
+        {
+           var margin = {top: 20, right: 20, bottom: 30, left: 40},
+                        width = 960 - margin.left - margin.right,
+                        height = 500 - margin.top - margin.bottom;
+
+            data.sort(function(a, b) { return b.count - a.count; });
+            
+            var x = d3.scaleBand()
+                    .range([0, width])
+                    .padding(0.1)
+                    .domain(data.map( function (d) {return d.letter;}))
+            var y = d3.scaleLinear()
+                    .range([height, 0])
+                    .domain([0, d3.max(data, function (d){return +d.count;})]);
+            
+            // append the svg object to the body of the page
+            // append a 'group' element to 'svg'
+            // moves the 'group' element to the top left margin
+            var svg = d3.select("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", 
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+            // Append bars
+            svg.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function (d){return x(d.letter);})
+                .attr("width", x.bandwidth())
+                .attr("y", function (d) {return y(d.count);})
+                .attr("height", function (d) { return  height - y(+d.count);});
+
+            // Add x-axis
+            // add the x Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+              // add the y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
+
+
+        })(letterCountArray)
+
 }
 
